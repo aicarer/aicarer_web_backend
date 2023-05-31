1ab72dbd2b56d3fbbadd08b2de5ad57160d5ff51
whineai <whine@aicarer.com.au>
2023-06-01 02:33:12 +0800

    garmin endpoints and user authentication plus callback url

--- index.js
+++ index.js
@@ -13,27 +13,81 @@ import { ApolloServerPluginInlineTrace } from "apollo-server-core";
 import url from 'url';
 import {mergeTypeDefs, mergeResolvers} from '@graphql-tools/merge'
 import { makeExecutableSchema }from '@graphql-tools/schema';
+import OAuth from 'oauth-1.0a';
+import axios from 'axios';
+import crypto from 'crypto';
+import session from 'express-session';
+
 
 //resolvers to be refactor later
 
 // import Advertisement from './controllers/v2/resolver/AdvertisementResolver.js'
 import Organization from './controllers/v2/resolver/OrganizationResolver.js'
 import Admin from './controllers/v2/resolver/AdminResolver.js'
+import Garmin from './controllers/v2/resolver/GarminResolvers.js'
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
+const consumerKey = process.env.GARMIN_CLIENT_ID;
+const consumerSecret = process.env.GARMIN_CLIENT_SECRET;
 
 app.use(cors({
     origin: '*',
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
+
+app.use(session({
+  secret: 'ai-carer-garmin',
+  resave: false,
+  saveUninitialized: true
+}));
+
+//garmin login authorization 
+app.get('/login', async (req, res) => {
+  // Step 1: Request temporary token
+  const oauth = new OAuth({
+    consumer: {
+      key: consumerKey,
+      secret: consumerSecret
+    },
+    signature_method: 'HMAC-SHA1',
+    hash_function: (baseString, key) =>
+      crypto.createHmac('sha1', key).update(baseString).digest('base64')
+  });
+
+  const requestData = {
+    url: 'https://connectapi.garmin.com/oauth-service/oauth/request_token',
+    method: 'POST'
+  };
+
+  const token = await axios.post(requestData.url, null, {
+    headers: oauth.toHeader(oauth.authorize(requestData))
+  });
+
+  const temporaryToken = token.data.split('&')[0].split('=')[1];
+  const temporaryTokenSecret = token.data.split('&')[1].split('=')[1];
+
+  console.log(temporaryToken)
+  console.log(temporaryTokenSecret)
+
+  req.session.temporaryTokenSecret = temporaryTokenSecret;
+  req.session.temporaryToken = temporaryToken;
+  // Step 2: Authorize application
+  const authorizeUrl = `https://connect.garmin.com/oauthConfirm?oauth_token=${temporaryToken}`;
+  
+  // Redirect user to the authorization URL
+  res.redirect(authorizeUrl);
+});
+
+// end
+
 const server = http.createServer(app);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const resolverArray = [Organization, Admin];
+const resolverArray = [Organization, Admin, Garmin];
 
 const typeDefs = mergeTypeDefs(
   loadFilesSync(path.join(__dirname, './TypeDefs/*.gql'))
@@ -88,6 +142,60 @@ const apolloServer = new ApolloServer({
     console.log(mongoose.connection.readyState)
     console.log('Connected to MongoDB');
 
+// Callback URL
+    app.get('/callback', async (req, res) => {
+
+      const temporaryToken = req.session.temporaryToken;
+      const temporaryTokenSecret = req.session.temporaryTokenSecret;
+      const oauthToken = req.query.oauth_token;
+      const oauth = new OAuth({
+          consumer: {
+            key: consumerKey,
+            secret: consumerSecret
+          },
+          signature_method: 'HMAC-SHA1',
+          hash_function: (baseString, key) =>
+            crypto.createHmac('sha1', key).update(baseString).digest('base64')
+      });
+  
+  
+      const oauthVerifier = req.query.oauth_verifier;
+  
+  
+      const accessRequestData = {
+          url: 'https://connectapi.garmin.com/oauth-service/oauth/access_token',
+          method: 'POST',
+          data: {
+              oauth_verifier: oauthVerifier
+          },
+          
+      };
+  
+      const whineToken = {
+          key: temporaryToken,
+          secret: temporaryTokenSecret
+      }
+  
+      const headers = oauth.toHeader(oauth.authorize(accessRequestData, whineToken));
+      console.log("WHINEYYY")
+      // console.log(headers)
+      const response = await axios.post(accessRequestData.url, null, { headers });
+  
+      const accessToken = response.data.split('&')[0].split('=')[1];
+      const accessTokenSecret = response.data.split('&')[1].split('=')[1];
+  
+      // console.log("WHINE LEGIT ACCESS TOKEN")
+      // console.log(accessToken)
+      // console.log("WHINE LEGIT TOKEN SECRET")
+      // console.log(accessTokenSecret)
+  
+      const result = {
+          accessToken,
+          accessTokenSecret
+      }
+      res.json(result)
+  
+    });
 
   server.listen({ port: process.env.PORT }, () =>
     console.log(`Server running on http://localhost:${process.env.PORT }${apolloServer.graphqlPath}`)
