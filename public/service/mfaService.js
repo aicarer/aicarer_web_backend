8dc646b0d9497895edb7e15cc8a558d51accd2ce
singhbalr1 <singhbalr1@gmail.com>
2023-06-12 12:36:45 +0800

    Update mfaService.js

--- public/service/mfaService.js
+++ public/service/mfaService.js
@@ -19,19 +19,19 @@ export const updateUser = async (user) => {
 export const sendMFAEmail = (email, mfaCode) => {
 
     const transporter = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
+        host: process.env.MFA_OUTGOING_HOST,
+        port: process.env.MFA_OUTGOING_PORT,
         auth: {
-          user: "9fd8e7279c0dd0",
-          pass: "be444ee145777d"
+          user: process.env.MFA_OUTGOING_EMAIL,
+          pass: process.env.MFA_OUTGOING_PASSWORD
         }
       });
 
     const mailData = `<h1>Your MFA code is: ${mfaCode}</h1>`
 
     const mailOptions = {
-        from: 'c7b53026d9-34c435@inbox.mailtrap.io',
-        name: 'c7b53026d9-34c435@inbox.mailtrap.io',
+        from: 'connect@aicarer.com.au',
+        name: 'connect@aicarer.com.au',
         to: email,
         subject: 'MFA Code',
         html: mailData
